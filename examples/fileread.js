bcd5f71abb3431fe1e67f979627aef4d49cc70e2
Hexagon <robinnilsson@gmail.com>
2016-10-01 00:02:00 +0200

    Force output on unhandled errors in flow

--- examples/fileread.js
+++ examples/fileread.js
@@ -1,13 +1,13 @@
 var 
 	// Initialize abstractor
-	flow = require('../lib')({ debug: true, verbose: false }),
+	flow = require('../lib')(),
 
 	// Create nodes
 	httpNode = flow( "http-server", { port: 8087 }),
 	fileNode = flow( "file-read" ),
 
 	// Application settings
-	wwwRoot = "/var/www/test/",					// <- including tailing slash
+	wwwRoot = "c:/",					// <- including tailing slash
 
 	// Request handler from generic 
 	requestHandler = flow( "generic", (msg, uri) => { msg.path = wwwRoot + uri; return msg;  });
