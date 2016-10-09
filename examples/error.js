302ae82023241e573e54b36cb18da9d1cea5c3a3
Hexagon <robinnilsson@gmail.com>
2016-10-10 00:31:42 +0200

    Cleanup, Colors in console, Version bump.

--- examples/error.js
+++ examples/error.js
@@ -1,3 +1,13 @@
 var 
     // Initialize abstractor
-    flow = require("../lib")('oops');
+    flow = require("../lib")({ debug: true });
+
+    genError = flow("generic", function (msg) { 
+    	this.log.warning("DEBUG", 'Woop');
+    	this.log.info("DEBUG", 'Hellu');
+    	this.log.error("DEBUG", 'Woah!',"lol");
+    	this.log.info("DEBUG", "Alpha");
+    	this.log.throw("DEBUG", "I'm outta here");
+    });
+
+genError.start({});
